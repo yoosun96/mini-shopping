0eda78773bc15e2c01cb6784457e36a60c55c5db
yoosun96 <yoosnu@naver.com>
2021-05-24 17:26:54 +0900

    final commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+
+// Fetch teh items from the JOSN file
+function loadItems() {
+    return fetch('data/data.json')
+        .then(response => response.json())
+        .then(json => json.items)
+}
+
+// Update the list with the given items
+// 아이템들을 리스트로 업데이트
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// create HTML list item form the given data item
+// 아이템들을 HTML 리스트로 만들기
+function createHTMLString(item) {
+    return `
+    <li class="item">
+       <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+       <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+    displayItems(items.filter(item => item[key] === value));
+}
+
+
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+// main
+loadItems()
+    .then(items => {
+        displayItems(items);
+        setEventListeners(items)
+    })
+    .catch(console.log);
